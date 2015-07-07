4e481086a45ad5566c283be72a74c867de1bc9bf
Sheyna <sheyna@users.noreply.github.com>
2015-07-07 02:21:01 -0700

    Working on Mobile menu. Put the scafolding in, will customize.

--- public/javascripts/dropDownMenu.js
+++ public/javascripts/dropDownMenu.js
@@ -17,3 +17,21 @@
 //   }
 // });
 
+// Todd Motto's Flawless clickable drop-down navigation
+// http://toddmotto.com/flawless-clickable-drop-down-navigation/
+
+$(function () {
+  $('.header-mobile > ul').toggleClass('no-js js');
+  $('.header-mobile .js ul').hide();
+  $('.header-mobile .js').click(function(e) {
+    $('.header-mobile .js ul').slideToggle(200);
+    $('.clicker').toggleClass('active');
+    e.stopPropagation();
+  });
+  $(document).click(function() {
+    if ($('.header-mobile .js ul').is(':visible')) {
+      $('.header-mobile .js ul', this).slideUp();
+      $('.clicker').removeClass('active');
+    }
+  });
+});
