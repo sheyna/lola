ed2c5e62b50937efd8a16b9b08760d9d69f93881
Sheyna <sheyna@users.noreply.github.com>
2015-07-12 17:01:53 -0700

    Ajusted dropdown js.

--- public/javascripts/dropDownMenu.js
+++ public/javascripts/dropDownMenu.js
@@ -1,26 +1,47 @@
-// $(function(){
-//   $('li.sub-subhead').hide();
-// });
-
-// $('li#li-subhead').on('click', function(){
-//   $('li.sub-subhead').show();
-//   $('li#li-subhead').on('click', function(){
-//     $('li.sub-subhead').hide();
-//   });
-// });
-
-// $('li#li-subhead').on('click', function(){
-//   if ($('li.sub-subhead').hide();) {
-//     $('li.sub-subhead').show();
-//   } else {
-//     $('li.sub-subhead').hide();
-//   }
-// });
-
 // Todd Motto's Flawless clickable drop-down navigation
 // http://toddmotto.com/flawless-clickable-drop-down-navigation/
 
+// // version 1:
+// var dropDown = function(menuUl, menuJsUl, menuJs, clicker, menuJsUlThis) {
+//   menuUl.toggleClass('no-js js');
+//   menuJsUl.hide();
+//   menuJs.click(function(e) {
+//     menuJsUl.slideToggle(200);
+//     clicker.toggleClass('active');
+//     e.stopPropagation();
+//   });
+//   $(document).click(function() {
+//     if (menuJsUl.is(':visible')) {
+//       menuJsUlThis.slideUp();
+//       clicker.removeClass('active');
+//     }
+//   });
+// });
+
+// // version 2:
+// var dropDown = function(menuUl, menuJsUl, menuJs, clicker) {
+//   $(menuUl).toggleClass('no-js js');
+//   $(menuJsUl).hide();
+//   $(menuJs).click(function(e) {
+//     $(menuJsUl).slideToggle(200);
+//     $(clicker).toggleClass('active');
+//     e.stopPropagation();
+//   });
+//   $(document).click(function() {
+//     if ($(menuJsUl).is(':visible')) {
+//       $(menuJsUl, this).slideUp();
+//       $(clicker).removeClass('active');
+//     }
+//   });
+// });
+
 $(function () {
+  // version 1 call:
+  // dropDown('$(\'.header-mobile > ul\')', '$(\'.header-mobile .js ul\')', '$(\'.header-mobile .js\')', '$(\'.clicker\')', '$(\'.header-mobile .js ul\', this)');
+
+  // version 2 call:
+  // dropDown('.header-mobile > ul', '.header-mobile .js ul', '.header-mobile .js', '.clicker');
+
   $('.header-mobile > ul').toggleClass('no-js js');
   $('.header-mobile .js ul').hide();
   $('.header-mobile .js').click(function(e) {
@@ -37,17 +58,19 @@ $(function () {
 });
 
 $(function () {
-  $('.drop-down-menu > ul').toggleClass('no-js js');
-  $('.drop-down-menu .js ul').hide();
-  $('.drop-down-menu .js').click(function(e) {
-    $('.drop-down-menu .js ul').slideToggle(200);
-    $('.drop-down-menu--clicker').toggleClass('active');
-    e.stopPropagation();
-  });
-  $(document).click(function() {
-    if ($('.drop-down-menu .js ul').is(':visible')) {
-      $('.drop-down-menu .js ul', this).slideUp();
-      $('.drop-down-menu--clicker').removeClass('active');
-    }
-  });
+  if ($(window).width() <= 450) {
+    $('.drop-down-menu > ul').toggleClass('no-js js');
+    $('.drop-down-menu .js ul').hide();
+    $('.drop-down-menu .js').click(function(e) {
+      $('.drop-down-menu .js ul').slideToggle(200);
+      $('.drop-down-menu--clicker').toggleClass('active');
+      e.stopPropagation();
+    });
+    $(document).click(function() {
+      if ($('.drop-down-menu .js ul').is(':visible')) {
+        $('.drop-down-menu .js ul', this).slideUp();
+        $('.drop-down-menu--clicker').removeClass('active');
+      }
+    });
+  }
 });
